e5eb40cf2755d539a44933311552ee8f60d1a882
Norimasa Tochibayashi <norimasa.tochibayashi@memius.com>
2017-02-22 12:59:13 +0900

    'views.py' was amended.

--- 2017.04/meus/views.py
+++ 2017.04/meus/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return  HttpResponse("Hello, Memius. You are now with Python.")
+    return HttpResponse("Hello, Memius. You are now with Python.")
 
 # Create your views here.
